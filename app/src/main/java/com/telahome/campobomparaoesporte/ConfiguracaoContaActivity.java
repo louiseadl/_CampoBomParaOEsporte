9c4d2012a1d817df1ec6dbff3ea300a949834bdb
LouiseALD <louisecarvalho2001@gmail.com>
2019-10-27 18:13:01 -0300

    Commit 11!

---
+++ app/src/main/java/com/telahome/campobomparaoesporte/ConfiguracaoContaActivity.java
@@ -0,0 +1,14 @@
+package com.telahome.campobomparaoesporte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracaoContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracao_conta);
+    }
+}
