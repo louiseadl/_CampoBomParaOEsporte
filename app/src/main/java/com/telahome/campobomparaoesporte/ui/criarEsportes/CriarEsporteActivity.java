6c0f7a46a8e468a2b6f46aeaaa765e9cde66b200
JoaoLucas <joaolucaskuhnahammes@gmail.com>
2019-11-02 01:02:57 -0300

    joao5

--- app/src/main/java/com/telahome/campobomparaoesporte/ui/criarEsportes/CriarEsporteActivity.java
+++ app/src/main/java/com/telahome/campobomparaoesporte/ui/criarEsportes/CriarEsporteActivity.java
@@ -10,11 +10,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.telahome.campobomparaoesporte.Models.Esporte;
-import com.telahome.campobomparaoesporte.Models.Usuario;
 import com.telahome.campobomparaoesporte.R;
 import com.telahome.campobomparaoesporte.RetrofitCreator;
 import com.telahome.campobomparaoesporte.Services.EsportesServices;
-import com.telahome.campobomparaoesporte.ui.register.Register2Activity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -48,20 +46,22 @@ public class CriarEsporteActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Esporte esporte = new Esporte(etNome.getText().toString(), etDescricao.getText().toString());
                 EsportesServices service = retrofit.create(EsportesServices.class);
-                service.setEsportes(esporte).enqueue(new Callback<Usuario>() {
+                service.setEsportes(esporte).enqueue(new Callback<Esporte>() {
                     @Override
-                    public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                    public void onResponse(Call<Esporte> call, Response<Esporte> response) {
                         if(response.isSuccessful()) {
-                            Toast.makeText(Register2Activity.this, "Usuario cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(CriarEsporteActivity.this, "Esporte cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                            Intent itVoltar = new Intent(CriarEsporteActivity.this,EsportesMainActivity.class);
+                            startActivity(itVoltar);
                         } else {
-                            Toast.makeText(Register2Activity.this, "Falha no cadastro, tente novamente mais tarde!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(CriarEsporteActivity.this, "Falha no cadastro, tente novamente mais tarde!", Toast.LENGTH_LONG).show();
                         }
 
                     }
 
                     @Override
-                    public void onFailure(Call<Usuario> call, Throwable t) {
-                        Toast.makeText(Register2Activity.this, "Falha na comunicação! Verifique sua internet e tente novamente!", Toast.LENGTH_LONG).show();
+                    public void onFailure(Call<Esporte> call, Throwable t) {
+                        Toast.makeText(CriarEsporteActivity.this, "Falha na comunicação! Verifique sua internet e tente novamente!", Toast.LENGTH_LONG).show();
                     }
                 });
 
